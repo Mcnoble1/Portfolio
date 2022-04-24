76edbe289a27a7d5fc4473776e594c0e1c52c40c
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-04-24 22:39:01 +0100

    feat: files cleanup :octocat:

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,13 @@
   "use strict";
 
   // Preloader
-  $(window).on('load', function() {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
-        $(this).remove();
-      });
-    }
-  });
+  // $(window).on('load', function() {
+  //   if ($('#preloader').length) {
+  //     $('#preloader').delay(100).fadeOut('slow', function() {
+  //       $(this).remove();
+  //     });
+  //   }
+  // });
 
   // Hero typed
   if ($('.typed').length) {
